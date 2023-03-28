7fba9e6a5956fee8cba8f5f4ce528e06a7703c67
Berkay Ozturk <berkay_ozturk0151@hotmail.com>
2023-03-28 14:42:43 +0300

    Girilen sayıları küçükten büyüğe sıralayan program

---
+++ SayilariKucuktenBuyugeSiralama.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SayilariKucuktenBuyugeSiralama {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1.Sayı:");
+        a = input.nextInt();
+
+        System.out.print("2.Sayı:");
+        b = input.nextInt();
+
+        System.out.print("3.Sayı:");
+        c = input.nextInt();
+
+        if (a<b && b<c){
+            System.out.println(a+"<"+b+"<"+c);
+        } else if (b<a && a<c) {
+            System.out.println(b+"<"+a+"<"+c);
+        } else if (a<c && c<b) {
+            System.out.println(a+"<"+c+"<"+b);
+        } else if (c<a && a<b) {
+            System.out.println(c+"<"+a+"<"+b);
+        } else if (c<b && b<a) {
+            System.out.println(c+"<"+b+"<"+a);
+        } else {
+            System.out.println(b+"<"+c+"<"+a);
+        }
+
+
+    }
+}
